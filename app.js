b2866382f9c784d73b08c383f0abd818eb07c1ad
Mourya Rajala <c_rajala.mourya@cerner.com>
2016-05-10 16:49:18 -0500

    Commiting Mongo DB Sample usage for Web app using node.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs')
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -18,7 +20,8 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
+app.use(logger('common', {stream: accessLogStream}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -33,6 +36,41 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+//Mongo DB connections
+mongoose.connect('mongodb://mourya:mourya@ds019472.mlab.com:19472/addressbook');
+
+//Delcaring Schema
+var Schema = mongoose.Schema;
+var personSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  Address: String
+});
+
+//Creating Model out of schema
+var Person = mongoose.model('Person', personSchema);
+
+//Creating instance like Person Model
+/*var john = Person({
+  firstName: 'Mourya',
+  lastName: 'Rajala',
+  Address: '12402 west'
+});
+
+//Saving the instance in the database.
+john.save(function(err){
+  console.log(err); 
+  console.log('Person is saved successFully');
+});*/
+
+
+Person.find({},(err,users)=>{
+  console.log(users);
+});
+
+
+
 
 // error handlers
 
